e738f35ae5e8d182935a2986b17eed98cd08ed54
Natalie Kaufhold <stu222510@mail.uni-kiel.de>
2021-12-01 17:58:24 +0100

    Rewrote count-three-measurement_002.py

--- 01-december/count-three-measurement_002.py
+++ 01-december/count-three-measurement_002.py
@@ -1,30 +1,28 @@
 input_file = open("test.txt", "r")
+lines = map(int, input_file.readlines())
+input_file.close
+
 count = 0
 
 cur_sum = 0
 prev_sum = 0
 
-#x = input_file.readlines()[1:4]
-#y = map(lambda i: int(i), x)
-#print(x)
+num_lines = len(lines)
+for i in range(num_lines):
+    #print(i)
 
-# my_iter = iter(input_file)
-# for (i, line) in zip(my_iter, input_file):
-#     print(i, line)
+    if i+2 <= num_lines:
+        cur_sum = sum([lines[i], lines[i+1], lines[i+2]])
+    else:
+        break
 
-i = 1
-for line in input_file:
-    cur_sum = sum(map(lambda x: int(x), input_file.readlines()[i:i+3]))
+    print(i, cur_sum)
 
-    print(cur_sum)
+    if cur_sum > prev_sum:
+        count = count + 1
 
-    # if cur_sum > prev_sum:
-    #     count = count + 1
-    #
-    # prev_sum = cur_sum
-    # # DO NOT forget to reset the sum!
-    # cur_sum = 0
+    prev_sum = cur_sum
+    # DO NOT forget to reset the sum!
+    cur_sum = 0
 
-    i += 1
-
-# print(count)
+print(count)
