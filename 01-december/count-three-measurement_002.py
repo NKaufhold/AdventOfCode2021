ccda8223501c3c6f183a28a2216767b0bc2eed25
Natalie Kaufhold <stu222510@mail.uni-kiel.de>
2021-12-01 18:11:06 +0100

    Fixed little things

--- 01-december/count-three-measurement_002.py
+++ 01-december/count-three-measurement_002.py
@@ -1,24 +1,22 @@
-input_file = open("test.txt", "r")
+#input_file = open("test.txt", "r")
+input_file = open('measurement.txt')
 lines = map(int, input_file.readlines())
 input_file.close
 
 count = 0
 
 cur_sum = 0
-prev_sum = 0
+prev_sum = None
 
 num_lines = len(lines)
 for i in range(num_lines):
-    #print(i)
-
-    if i+2 <= num_lines:
+    if i < num_lines and i+1 < num_lines and i+2 < num_lines:
         cur_sum = sum([lines[i], lines[i+1], lines[i+2]])
+        print(i, cur_sum)
     else:
         break
 
-    print(i, cur_sum)
-
-    if cur_sum > prev_sum:
+    if prev_sum and cur_sum > prev_sum:
         count = count + 1
 
     prev_sum = cur_sum
