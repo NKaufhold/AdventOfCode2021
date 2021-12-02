3047ece047541c8689af316a4d39caaa9f511866
Natalie Kaufhold <stu222510@mail.uni-kiel.de>
2021-12-02 21:21:26 +0100

    Deleted redundant code

--- 01-december/count-three-measurement_002.py
+++ 01-december/count-three-measurement_002.py
@@ -1,5 +1,5 @@
-#input_file = open("test.txt", "r")
-input_file = open('measurement.txt')
+input_file = open("test.txt", "r")
+#input_file = open('measurement.txt')
 lines = map(int, input_file.readlines())
 input_file.close
 
@@ -9,12 +9,8 @@ cur_sum = 0
 prev_sum = None
 
 num_lines = len(lines)
-for i in range(num_lines):
-    if i < num_lines and i+1 < num_lines and i+2 < num_lines:
-        cur_sum = sum([lines[i], lines[i+1], lines[i+2]])
-        print(i, cur_sum)
-    else:
-        break
+for i in range(num_lines-2):
+    cur_sum = sum([lines[i], lines[i+1], lines[i+2]])
 
     if prev_sum and cur_sum > prev_sum:
         count = count + 1
