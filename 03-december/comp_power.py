7bb98e385d8ba8fa7b3410a7b080774c96c7c100
Natalie Kaufhold <stu222510@mail.uni-kiel.de>
2021-12-03 21:32:03 +0100

    Added method to convert to decimal

--- 03-december/comp_power.py
+++ 03-december/comp_power.py
@@ -1,3 +1,11 @@
+# Function to calculate the decimal value of a binary number:
+def convert(binary_number):
+    result = 0
+    for i in range(len(binary_number)):
+        result += binary_number[i] * 2**(len(binary_number)-1-i)
+    return result
+
+
 input_file = open("test-report.txt")
 #input_file = open("diagnostic-report.txt")
 
@@ -12,15 +20,21 @@ for line in input_file:
     if not count_num:
         count_num = [int(char) for char in line if char != '\n']
     else:
+        # Challenge: Could we do this with only one list comprehension?
         chars = [int(char) for char in line if char != '\n']
         for i in range(len(count_num)):
             count_num[i] += chars[i]
+
 
 print(count_num)
 
 # Let's calculate the binary values of the rates:
 gamma_rate_bin = [1 if one > (count_line-one) else 0 for one in count_num]
 print(gamma_rate_bin)
+gamma_rate = convert(gamma_rate_bin)
+print(gamma_rate)
 
 epsilon_rate_bin = [1 if g == 0 else 0 for g in gamma_rate_bin]
 print(epsilon_rate_bin)
+epsilon_rate = convert(epsilon_rate_bin)
+print(epsilon_rate)
